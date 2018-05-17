dace87f215d70bf65cf7ab82c400c69e24c6cde8
Wasif Khan <waasif.khaan@gmail.com>
2018-05-17 20:14:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,25 +1,23 @@
 class Triangle
   attr_reader :one, :two, :three
-def initialize (side_1 , side_2 , side_3)
-  @one = side_1
-  @two = side_2
-  @three = side_3
-end 
-def kind
-  if @one == @two || @one == @three || @two == @three 
-    equilateral.to_sym
-  else if @one == @two || @one == @three || @two == @three
-    isosceles.to_sym
-  else 
-    
-  if  @one + @two < @three || @one + @three < @two || @two + @three < @one || @one = 0 || @two = 0 || @three = 0
+  def initialize (side_1 , side_2 , side_3)
+    @one = side_1
+    @two = side_2
+    @three = side_3
+  end 
+  def kind
+    if @one == @two || @one == @three || @two == @three 
+      equilateral.to_sym
+    else if @one == @two || @one == @three || @two == @three
+      isosceles.to_sym
+    else if @one + @two < @three || @one + @three < @two || @two + @three < @one || @one = 0 || @two = 0 || @three = 0
     begin
       raise TriangleError
     rescue TriangleError => error
       puts error.message
     end
-  end 
-  scalene.to_sym
+    else  
+      scalene.to_sym
   end
 end 
 end 
