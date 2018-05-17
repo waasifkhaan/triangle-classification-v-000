bab5bfe4e7cf39447df6f67e50ac2aec7ef0a5be
Wasif Khan <waasif.khaan@gmail.com>
2018-05-17 20:41:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   def kind
     
     if @one + @two < @three || @one + @three < @two || @two + @three < @one || @one == 0 || @two == 0 || @three == 0
-      #binding.pry
+      binding.pry
       begin
         raise TriangleError
       #rescue TriangleError => error
